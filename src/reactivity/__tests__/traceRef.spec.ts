405774a70a09b72a5de405ef83f3ccb87f06e06e
wangtao0101 <yuecjn@gmail.com>
2020-01-08 13:20:20 +0800

    feat: trace computed value

--- src/reactivity/__tests__/traceRef.spec.ts
+++ src/reactivity/__tests__/traceRef.spec.ts
@@ -1,9 +1,10 @@
 import { unlockValue, lockValue } from '../lock';
-import { reactiveTrace } from '../traceRef';
+import { reactiveTrace, computedTrace } from '../traceRef';
 import { reactive } from '../reactive';
 import { stop } from '../effect';
+import { computed } from '../computed';
 
-describe('reactivity/state', () => {
+describe('reactivity/traceRef', () => {
   beforeAll(() => {
     unlockValue();
   });
@@ -12,18 +13,18 @@ describe('reactivity/state', () => {
     lockValue();
   });
 
-  it('should trigger componentState scheduler', () => {
+  it('should trigger reactive trace scheduler', () => {
     const original = { foo: 1 };
     const observed = reactive(original);
 
     const fnSpy = jest.fn(() => {});
 
-    const componentState = reactiveTrace(original, fnSpy);
+    const traceRef = reactiveTrace(original, fnSpy);
 
-    expect(componentState.value.foo).toBe(1);
+    expect(traceRef.value.foo).toBe(1);
     observed.foo++;
 
-    expect(componentState.value.foo).toBe(2);
+    expect(traceRef.value.foo).toBe(2);
     expect(fnSpy).toHaveBeenCalledTimes(1);
   });
 
@@ -32,31 +33,70 @@ describe('reactivity/state', () => {
 
     const fnSpy = jest.fn(() => {});
 
-    const componentState = reactiveTrace(original, fnSpy);
+    const traceRef = reactiveTrace(original, fnSpy);
 
     expect(() => {
-      delete componentState.value.foo;
+      delete traceRef.value.foo;
     }).toThrowError(/Cannot delete key: foo of hux state except in reducer./);
 
     expect(() => {
-      componentState.value.foo = 2;
+      traceRef.value.foo = 2;
     }).toThrowError(/Cannot set key: foo of hux state except in reducer./);
   });
 
-  it('should no longer call scheduler when stopped', () => {
+  it('should no longer call scheduler when reactive trace effect stopped', () => {
     const original = { foo: 1 };
     const observed = reactive(original);
 
     const fnSpy = jest.fn(() => {});
 
-    const componentState = reactiveTrace(original, fnSpy);
+    const traceRef = reactiveTrace(original, fnSpy);
 
-    expect(componentState.value.foo).toBe(1);
+    expect(traceRef.value.foo).toBe(1);
     observed.foo++;
 
     expect(fnSpy).toHaveBeenCalledTimes(1);
 
-    stop(componentState.effect);
+    stop(traceRef.effect);
+
+    observed.foo++;
+
+    expect(fnSpy).toHaveBeenCalledTimes(1);
+  });
+
+  it('should trigger computed trace scheduler', () => {
+    const original = { foo: 1 };
+    const observed = reactive(original);
+
+    const cValue = computed(() => observed.foo + 1);
+
+    const fnSpy = jest.fn(() => {});
+
+    const traceRef = computedTrace(cValue, fnSpy);
+
+    expect(traceRef.value).toBe(2);
+    observed.foo++;
+
+    expect(traceRef.value).toBe(3);
+    expect(fnSpy).toHaveBeenCalledTimes(1);
+  });
+
+  it('should no longer call scheduler when computed trace effect stopped', () => {
+    const original = { foo: 1 };
+    const observed = reactive(original);
+
+    const cValue = computed(() => observed.foo + 1);
+
+    const fnSpy = jest.fn(() => {});
+
+    const traceRef = computedTrace(cValue, fnSpy);
+
+    expect(traceRef.value).toBe(2);
+    observed.foo++;
+
+    expect(fnSpy).toHaveBeenCalledTimes(1);
+
+    stop(traceRef.effect);
 
     observed.foo++;
 
--- src/reactivity/traceRef.ts
+++ src/reactivity/traceRef.ts
@@ -2,13 +2,16 @@ import { ReactiveEffectType, effect, ReactiveEffect } from './effect';
 import { NOOP } from '../utils';
 import { reactive } from './reactive';
 import { Ref, UnwrapRef } from './ref';
+import { ComputedRef } from './computed';
 
-export interface ReactiveTraceRef<T = any> extends Ref<T> {
+export interface TraceRef<T = any> extends Ref<T> {
   readonly effect: ReactiveEffect<T>;
   readonly value: UnwrapRef<T>;
 }
 
-export function reactiveTrace<T>(target: T, scheduler: () => void): ReactiveTraceRef<T> {
+// export function createTraceEffect
+
+export function reactiveTrace<T>(target: T, scheduler: () => void): TraceRef<T> {
   const runner = effect(NOOP, {
     lazy: true,
     // mark effect as trace effect so that it gets low priority during trigger
@@ -25,5 +28,34 @@ export function reactiveTrace<T>(target: T, scheduler: () => void): ReactiveTrac
     // expose effect so trace ref can be stopped
     effect: runner,
     value
+  } as any;
+}
+
+export function computedTrace<T>(computedRef: ComputedRef, scheduler: () => void): TraceRef<T> {
+  const runner = effect(NOOP, {
+    lazy: true,
+    // mark effect as trace effect so that it gets low priority during trigger
+    type: ReactiveEffectType.TRACE,
+    scheduler: () => {
+      scheduler();
+    }
+  });
+
+  return {
+    _isRef: true,
+    // expose effect so trace ref can be stopped
+    effect: runner,
+    get value() {
+      const value = computedRef.value;
+      const childRunner = computedRef.effect;
+      for (let i = 0; i < childRunner.deps.length; i++) {
+        const dep = childRunner.deps[i];
+        if (!dep.has(runner)) {
+          dep.add(runner);
+          runner.deps.push(dep);
+        }
+      }
+      return value;
+    }
   } as any;
 }
