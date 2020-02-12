85a3deb105ff3f59079e070c852dfcf1e441d9b0
wangtao0101 <yuecjn@gmail.com>
2020-02-12 17:59:45 +0800

    build: api-extractor

--- scripts/build.js
+++ scripts/build.js
@@ -1,11 +1,27 @@
 const path = require('path');
+const fs = require('fs-extra');
+const chalk = require('chalk');
 const { Extractor, ExtractorConfig } = require('@microsoft/api-extractor');
 
-const extractorConfigPath = path.resolve(process.cwd(), `api-extractor.json`);
-const extractorConfig = ExtractorConfig.loadFileAndPrepare(extractorConfigPath);
-const result = Extractor.invoke(extractorConfig, {
-  localBuild: true,
-  showVerboseMessages: true
-});
+run();
 
-console.log(result);
+async function run() {
+  const extractorConfigPath = path.resolve(process.cwd(), `api-extractor.json`);
+  const extractorConfig = ExtractorConfig.loadFileAndPrepare(extractorConfigPath);
+  const result = Extractor.invoke(extractorConfig, {
+    localBuild: true,
+    showVerboseMessages: true
+  });
+
+  if (result.succeeded) {
+    console.log(chalk.bold(chalk.green(`API Extractor completed successfully.`)));
+  } else {
+    console.error(
+      `API Extractor completed with ${extractorResult.errorCount} errors` +
+        ` and ${extractorResult.warningCount} warnings`
+    );
+    process.exitCode = 1;
+  }
+
+  await fs.remove(`dist/src`);
+}
